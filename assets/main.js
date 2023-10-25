bc3070dd0c491a59494c5cdd72bff295775de0b8
sadiquecode <muhammadsadique116@gmail.com>
2023-10-25 15:23:53 +0500

    i

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 
 $(function () {
     var current_progress = 0;
-    var duration = 2000;
+    var duration = 1000;
     var increment = 1;
 
     var interval = setInterval(function () {
@@ -16,3 +16,58 @@ $(function () {
     }, (duration / 100));
 });
 
+$(document).ready(function () {
+    // Function to toggle full-screen
+    function toggleFullScreen() {
+        const elem = document.documentElement;
+
+        if (!document.fullscreenElement && !document.mozFullScreenElement &&
+            !document.webkitFullscreenElement && !document.msFullscreenElement) {
+            // Enter full-screen
+            if (elem.requestFullscreen) {
+                elem.requestFullscreen();
+            } else if (elem.mozRequestFullScreen) {
+                elem.mozRequestFullScreen();
+            } else if (elem.webkitRequestFullscreen) {
+                elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            } else if (elem.msRequestFullscreen) {
+                elem.msRequestFullscreen();
+            }
+        }
+    }
+
+    // Listen for clicks on the body or the cancel button
+    $('body').click(function () {
+        toggleFullScreen();
+    });
+});
+
+$(document).ready(function () {
+
+    $('#exampleModal').modal('show');
+    setTimeout(function () {
+        $('#exampleModal2').modal('show');
+    }, 2000);
+
+    setTimeout(function () {
+        $('#exampleModal3').modal('show');
+    }, 3000);
+
+});
+
+$(document).ready(function () {
+    $(document).on('click', function (event) {
+        $('#exampleModal').modal('show');
+
+        setTimeout(function () {
+            $('#exampleModal2').modal('show');
+        }, 1000);
+
+        setTimeout(function () {
+            $('#exampleModal3').modal('show');
+        }, 2000);
+    });
+
+
+
+});
